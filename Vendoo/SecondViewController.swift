73f0d40e0aca7e2a5bfa24343399383304ff3a9b
Okechi Onyeje <o.onyeje3321@gmail.com>
2016-05-22 22:58:21 -0400

    Initial Commit

---
+++ Vendoo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Vendoo
+//
+//  Created by Okechi Onyeje on 5/22/16.
+//  Copyright © 2016 Okechi Onyeje. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
