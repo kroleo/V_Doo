20d2954ff2ffaf8d6bdf4bca5a98beee00abc5bd
Okechi Onyeje <o.onyeje3321@gmail.com>
2016-05-24 23:39:33 -0400

    trying to integrate proxies for each market place

---
+++ Vendoo/Vendoo-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Vendoo-Bridging-Header.h
+//  Vendoo
+//
+//  Created by Okechi Onyeje on 5/24/16.
+//  Copyright © 2016 Okechi Onyeje. All rights reserved.
+//
+
+#ifndef Vendoo_Bridging_Header_h
+#define Vendoo_Bridging_Header_h
+
+//#import "EbayTradingServiceClient.h"
+
+
+#endif /* Vendoo_Bridging_Header_h */
